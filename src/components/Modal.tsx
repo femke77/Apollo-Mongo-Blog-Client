1fdfb2d67db5a9b742448f12a3062876b4ec311b
femke77 <megan.meyers.388@gmail.com>
2025-01-14 16:40:14 -0800

    added href to render in modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,32 +6,29 @@ import { useState, useRef } from 'react';
 
 function HideModal() {
     const checkRef = useRef<HTMLInputElement>(null);
-    
+
     const [show, setShow] = useState(true);
-    const handleClose = () =>{
+    const handleClose = () => {
         setShow(false);
         if (checkRef.current?.checked) {
             document.cookie = "hidePopup=true; path=/; max-age=31536000";
-            
         }
     }
-    
-  return (
-    <>
-      <Modal show={show} onHide={handleClose}>
-    
 
-        <Modal.Body>
-          <p>Due to the server being a free app on <a href="https://www.render.com" target='_blank'>Render.com</a>, initial load times maybe be 50+ seconds.</p>
-        </Modal.Body>
+    return (
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Body>
+                    <p>Due to the server being a free app on <a href="https://www.render.com" target='_blank'>Render.com</a>, initial load times maybe be 50+ seconds.</p>
+                </Modal.Body>
 
-        <Modal.Footer className="d-flex justify-content-between">
-          <FormCheck ref={checkRef} type="checkbox" label="Don't show this again" />
-          <Button variant="secondary" onClick={handleClose}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+                <Modal.Footer className="d-flex justify-content-between">
+                    <FormCheck ref={checkRef} defaultChecked={false} type="checkbox" label="Don't show this message again. (Uses cookies)" />
+                    <Button variant="primary" onClick={handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
 
 export default HideModal;
